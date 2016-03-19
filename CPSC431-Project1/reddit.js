15b1b9489d80e7405700d56a11e7f698c4d2ab3e
niyatibichu <niyatibichu@csu.fullerton.edu>
2016-03-19 12:50:16 -0700

    ADDED CSS STYLES

---
+++ CPSC431-Project1/reddit.js
@@ -0,0 +1,53 @@
+var main = function() {
+    "use strict";
+    function myfunction() {
+        $.get("http://localhost:3000/reddit", function(getData){
+        getData.forEach(function(reddit) {
+       var imgId=reddit.id;
+			 var postsList ="<br><div class ='postContent'>"
+							+"<div class = 'votes'>"
+			                +"<img  id="+imgId+" class='voteUpButton' src='image/up.png'>"+"<br>"
+                            +"<strong id ="+reddit.id+" class='votesNum'>"+"</strong>"+"<br>"
+                            +"<img  id="+imgId+" class='voteDownButton' src='image/down.png'>"
+							+"</div>"
+							+"<div class ='Content-List'>"
+							+"<p>"+"<a href="+JSON.stringify(reddit.main_link)+">"+reddit.link_title+"</a>"
+							+"</p>"
+							+"</div>";
+			  $(postsList).appendTo('div.postsContainer');
+
+			  $("#"+reddit.id+".votesNum").text(reddit.likes);
+      });//end of ForEach loop
+
+      //Vote up event:
+      $("img.voteUpButton").on("click", function(){
+           var $imgId= this.id, main_link, link_title, image_link ,result=$("#"+this.id+".votesNum").text();
+           result++;// result is the number of likes after increasing or decreasing
+           $("#"+this.id+".votesNum").text(result);//updating the html with the new "Likes" value
+
+           //get request to get other reddit.json elements by using only the ID:
+             main_link=getData[this.id -1].main_link;
+             link_title=getData[this.id -1].link_title;
+             image_link=getData[this.id -1].image_link;
+             $.ajax({type:"PUT", url:"http://localhost:3000/reddit/"+ $imgId, data:{"id":$imgId,"image_link":image_link, "likes":result,"link_title":link_title ,"main_link":main_link, "post":"submitted"} });
+      });
+
+      //Vote down event:
+      $("img.voteDownButton").on("click",function(){
+          var $imgId= this.id, main_link, link_title, image_link, result=$("#"+this.id+".votesNum").text();
+          result--;// result is the number of likes after increasing or decreasing
+          $("#"+this.id+".votesNum").text(result);//updating the html with the new "Likes" value
+
+          //get request to get other reddit.json elements by using only the ID:
+            main_link=getData[this.id -1].main_link;
+            link_title=getData[this.id -1].link_title;
+            image_link=getData[this.id -1].image_link;
+            $.ajax({type:"PUT", url:"http://localhost:3000/reddit/"+ $imgId, data:{"id":$imgId,"image_link":image_link, "likes":result,"link_title":link_title ,"main_link":main_link, "post":"submitted"} });
+      });
+
+	 });//end of $.get function
+	}
+	 myfunction();
+    };
+
+$(document).ready(main);
